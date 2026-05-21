48e96512ade39bedddf77d87baac9961a7eada71
chenanqi <3290645156@qq.com>
2026-05-21 07:35:21 +0000

    feat: example

---
+++ example/callee/userservice.cc
@@ -0,0 +1,36 @@
+#include "user.pb.h"
+
+class UserService : fixbug::UserServiceRpc
+{
+public:
+	bool Login(const std::string& name, const std::string& pwd)
+	{
+		std::cout << "waiting DB" << std::endl;
+		std::cout << "name = " << name << " pwd =" << pwd << std::endl;
+		return true;
+	}
+
+	// 执行业务逻辑
+	void Login(::google::protobuf::RpcController* controller,
+			   const ::fixbug::LoginRequest* request,
+			   ::fixbug::LoginResponse* response,
+			   ::google::protobuf::Closure* done)
+	{
+		std::string name = request->name();
+		std::string pwd = request->pwd();
+
+		bool result = Login(name, pwd);
+		response->set_success(result);
+		response->mutable_result()->set_errcode(0);
+		response->mutable_result()->set_errmsg("");
+
+		done->Run();
+	}
+};
+
+
+int main()
+{
+
+	return 0;
+}
