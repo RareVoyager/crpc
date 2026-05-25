8ee8e34602386e0d12d9036af1c2da5c1221f817
chenanqi <3290645156@qq.com>
2026-05-25 05:34:27 +0000

    feat: 新增application,

--- crpc/include/crpcapplication.h
+++ crpc/include/crpcapplication.h
@@ -1,18 +1,27 @@
 #pragma once
 
-
-// crpc框架基础类 初始化操作
-class CrpcApplication
+#include <include/crpcconfig.h>
+namespace crpc
 {
-public:
-	static void Init(int argc, char** argv);
-    // 单例模式
-	static CrpcApplication* GetCrpcApplication();
+	// crpc框架基础类 初始化操作
+	class CrpcApplication
+	{
+	public:
+		static void Init(int argc, char** argv);
+		// 单例模式
+		static CrpcApplication& GetInstance();
 
-private:
-	CrpcApplication();
-	// 单例模式，将拷贝构造等删除
-	CrpcApplication(const CrpcApplication&) = delete;
-	CrpcApplication(const CrpcApplication&&) = delete;
-	CrpcApplication operator=(const CrpcApplication&) = delete;
-};
+		static CrpcConfig& GetConfig();
+
+	private:
+		static CrpcConfig config_;
+		CrpcApplication()
+		{
+		}
+		// 单例模式，将拷贝构造等删除
+		CrpcApplication(const CrpcApplication&) = delete;
+		CrpcApplication(const CrpcApplication&&) = delete;
+		CrpcApplication operator=(const CrpcApplication&) = delete;
+	};
+
+}// namespace crpc
--- crpc/src/crpcapplication.cc
+++ crpc/src/crpcapplication.cc
@@ -1,2 +1,66 @@
+#include <iostream>
+#include <unistd.h>
+
 #include <include/crpcapplication.h>
 
+void ShowArgHelp()
+{
+}
+
+
+namespace crpc
+{
+	CrpcConfig CrpcApplication::config_;
+	void CrpcApplication::Init(int argc, char** argv)
+	{
+		// 读取config.conf
+		if (argc < 2)
+		{
+			ShowArgHelp();
+			exit(EXIT_FAILURE);
+		}
+
+		int c = 0;
+		std::string config_file;
+		// argv 里查找 -i 这个选项，并且 -i 后面必须带一个参数 参数保存到 optarg这个全局变量中
+		while ((c = ::getopt(argc, argv, "i:")) != -1)
+		{
+			switch (c)
+			{
+			case 'i':
+				config_file = optarg;
+				break;
+			case '?':
+				std::cout << "invalid args!" << std::endl;
+				ShowArgHelp();
+				exit(EXIT_FAILURE);
+			case ':':
+				std::cout << "need <configfile>" << std::endl;
+				ShowArgHelp();
+				exit(EXIT_FAILURE);
+			default:
+				break;
+			}
+
+			// todo: 读取配置
+			config_.LoadConfigFile(config_file.c_str());
+
+			std::cout << "rpcserverip:" << config_.Load("rpcserverip") << std::endl;
+			std::cout << "rpcserverport:" << config_.Load("rpcserverport") << std::endl;
+			std::cout << "zookeeperip" << config_.Load("zookeeperip") << std::endl;
+			std::cout << "zookeeperport:" << config_.Load("zookeeperport") << std::endl;
+		}
+	}
+
+	CrpcApplication& CrpcApplication::GetInstance()
+	{
+		static CrpcApplication application;
+		return application;
+	}
+
+	CrpcConfig& CrpcApplication::GetConfig()
+	{
+		return config_;
+	}
+
+}// namespace crpc
