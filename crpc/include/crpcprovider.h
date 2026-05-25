a58161135c45caf8de79cedc237d91a28c5403d7
chenanqi <3290645156@qq.com>
2026-05-25 05:37:51 +0000

    feat: rpc提供者

--- crpc/include/crpcprovider.h
+++ crpc/include/crpcprovider.h
@@ -1,7 +1,42 @@
 #pragma once
+#include <unordered_map>
 
-class CrpcProvider
+
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/service.h>
+#include <muduo/net/Callbacks.h>
+#include <muduo/net/EventLoop.h>
+
+namespace crpc
 {
-public:
-private:
-};
+	// 框架提供的专门发布rpc服务的网络对象类
+	class CrpcProvider
+	{
+	public:
+		// 框架提供给为外部使用的，发布rpc方法的接口。所有rpc方法都集成自Service
+		void NotifyService(google::protobuf::Service* service);
+
+		// 启动rpc服务节点，提供rpc远程网络调用服务
+		void Run();
+
+	private:
+		void OnConnection(const muduo::net::TcpConnectionPtr&);
+
+		void OnMessage(const muduo::net::TcpConnectionPtr&,
+					   muduo::net::Buffer*,
+					   muduo::Timestamp);
+
+	private:
+		muduo::net::EventLoop loop_;
+
+		struct ServiceInfo
+		{
+			std::string service_name;
+			std::unordered_map<std::string, const google::protobuf::MethodDescriptor*> methodMap_;
+		};
+
+		std::unordered_map<std::string, ServiceInfo>
+				serviceInfoMap_;
+	};
+
+}// namespace crpc
--- crpc/src/crpcprovider.cc
+++ crpc/src/crpcprovider.cc
@@ -1,1 +1,61 @@
+
+#include <include/crpcapplication.h>
 #include <include/crpcprovider.h>
+#include <muduo/net/TcpServer.h>
+
+namespace crpc
+{
+	void CrpcProvider::NotifyService(google::protobuf::Service* service)
+	{
+		ServiceInfo service_info;
+		// google::protobuf::ServiceDescriptor 类型
+		const auto service_descriptor = service->GetDescriptor();
+
+		// 服务的名字
+		std::string service_name = service_descriptor->name();
+		// 服务的方法数量
+		int methodCnt = service_descriptor->method_count();
+
+		for (int i = 0; i < methodCnt; i++)
+		{
+			const google::protobuf::MethodDescriptor* method = service_descriptor->method(i);
+			std::string methodName = method->name();
+			service_info.methodMap_.insert({methodName, method});
+		}
+
+		serviceInfoMap_.insert({service_name, service_info});
+	}
+
+	void CrpcProvider::Run()
+	{
+		std::string ip = CrpcApplication::GetConfig().Load("rpcserverip");
+		uint16_t port = stoi(CrpcApplication::GetConfig().Load("rpcserverport"));
+		muduo::net::InetAddress address(ip, port, false);
+		muduo::net::TcpServer server(&loop_, address, "rpcProvider");
+
+		server.setThreadNum(3);
+
+		server.setConnectionCallback([this](const muduo::net::TcpConnectionPtr& conn) {
+			OnConnection(conn);
+		});
+
+		server.setMessageCallback([this](const muduo::net::TcpConnectionPtr& conn,
+										 muduo::net::Buffer* buffer,
+										 muduo::Timestamp reciveTime) {
+			OnMessage(conn, buffer, reciveTime);
+		});
+		server.start();
+		loop_.loop();
+	}
+
+	void CrpcProvider::OnConnection(const muduo::net::TcpConnectionPtr& conn)
+	{
+	}
+
+
+	void CrpcProvider::OnMessage(const muduo::net::TcpConnectionPtr& conn,
+								 muduo::net::Buffer* buffer,
+								 muduo::Timestamp reciveTime)
+	{
+	}
+}// namespace crpc
